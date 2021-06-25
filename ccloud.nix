aad3d1213756b40c191e0bf93ac5980a6baf71ee
Kris Jenkins <kjenkins@confluent.io>
2021-06-25 12:59:10 +0100

    Pinning the ccloud version correctly.

--- ccloud.nix
+++ ccloud.nix
@@ -1,10 +1,11 @@
 { pkgs }:
 
-pkgs.stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation rec {
   name = "confluent-cloud-cli";
+  version = "1.25.0";
   src = pkgs.fetchurl {
-    url = "https://s3-us-west-2.amazonaws.com/confluent.cloud/ccloud-cli/archives/latest/ccloud_latest_darwin_amd64.tar.gz";
-    sha256 = "0zpdyz1aa2xgcrn4465wjlj8h4ylbh8p237dc7hap0nl97y05shf";
+    url = "https://s3-us-west-2.amazonaws.com/confluent.cloud/ccloud-cli/archives/${version}/ccloud_v${version}_darwin_amd64.tar.gz";
+    sha256 = "0306jg36dpccwyy239r2xvw3bvsrnrdc88390g26fhcb0048qmgb";
   };
 
   dontFixup = true;

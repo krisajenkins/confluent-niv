4c27e439224a8561896542a21c81e1a2565cf270
Kris Jenkins <kjenkins@confluent.io>
2021-09-06 15:36:50 +0100

    Updating to ccloud 1.39.0.

--- ccloud.nix
+++ ccloud.nix
@@ -2,10 +2,10 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "confluent-cloud-cli";
-  version = "1.25.0";
+  version = "1.39.0";
   src = pkgs.fetchurl {
     url = "https://s3-us-west-2.amazonaws.com/confluent.cloud/ccloud-cli/archives/${version}/ccloud_v${version}_darwin_amd64.tar.gz";
-    sha256 = "0306jg36dpccwyy239r2xvw3bvsrnrdc88390g26fhcb0048qmgb";
+    sha256 = "0jqpmnx3izl4gv02zpx03z6ayi3cb5if4rnyl1374yaclx44k1gd";
   };
 
   dontFixup = true;

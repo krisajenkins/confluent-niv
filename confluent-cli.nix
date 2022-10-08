86083d2263cbb6aec605ac5cf7d662f8b06569aa
Kris Jenkins <kjenkins@confluent.io>
2022-10-08 16:10:52 +0100

    Updating to Confluent CLI 2.28.1.

--- confluent-cli.nix
+++ confluent-cli.nix
@@ -2,10 +2,10 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "confluent-cli";
-  version = "2.8.0";
+  version = "2.28.1";
   src = pkgs.fetchurl {
     url = "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/${version}/confluent_v${version}_darwin_amd64.tar.gz";
-    sha256 = "0ij3m85x0phsmj8kxcnxhxgwha5pvpamqc0qkhfl3dwgdwjcpsqk";
+    sha256 = "1mvg1z2hkims7x0zwsnhwf46dzgly97ci2gd8hd0730l9v4cig4n";
   };
 
   dontFixup = true;
